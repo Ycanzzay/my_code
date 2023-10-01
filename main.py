2e21f378b0a7a0790cef03377f341fd3adb3080c
Ycanzzay <steam3bebra228@gmail.com>
2023-10-01 23:34:06 +0300

    Calculator

---
+++ main.py
@@ -0,0 +1,20 @@
+print('calculator')
+a = int(input('Введите число'))
+b = int(input('Введите число'))
+c = input('Введите символ + - * ** / // %')
+if c == '+':
+    print('a', '+', 'b', '=', a + b)
+if c == '-':
+    print('a', '-', 'b', '=', a - b)
+if c == '*':
+    print('a', '*', 'b', '=', a * b)
+if c == '**':
+    print('a', '**', 'b', '=', a ** b)
+if c == '/':
+    print('a', '/', 'b', '=', a / b)
+if c == '//':
+    print('a', '//', 'b', '=', a // b)
+if c == '%':
+    print('a', '%', 'b', '=', a % b)
+
+

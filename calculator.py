5ccc4c3f11348139c5805940bc875bd05d4de225
Ycanzzay <steam3bebra228@gmail.com>
2023-10-17 22:06:32 +0300

    my first calculator

--- calculator.py
+++ calculator.py
@@ -1,18 +1,34 @@
 print('calculator')
-a = int(input('Введите число'))
-b = int(input('Введите число'))
-c = input('Введите символ + - * ** / // %')
-if c == '+':
-    print('a', '+', 'b', '=', a + b)
-if c == '-':
-    print('a', '-', 'b', '=', a - b)
-if c == '*':
-    print('a', '*', 'b', '=', a * b)
-if c == '**':
-    print('a', '**', 'b', '=', a ** b)
-if c == '/':
-    print('a', '/', 'b', '=', a / b)
-if c == '//':
-    print('a', '//', 'b', '=', a // b)
-if c == '%':
-    print('a', '%', 'b', '=', a % b)
+while True:
+    a = int(input('Введите число a: '))
+    b = int(input('Введите число b: '))
+    c = input('Введите символ + - * ** / // % или "v" для выхода')
+
+    if c == 'v':
+        break
+
+    if c == '+':
+        print(a, '+', b, '=', a + b)
+    elif c == '-':
+        print(a, '-', b, '=', a - b)
+    elif c == '*':
+        print(a, '*', b, '=', a * b)
+    elif c == '**':
+        print(a, '**', b, '=', a ** b)
+    elif c == '/':
+        if b != 0:
+            print(a, '/', b, '=', a / b)
+        else:
+            print("Ошибка: деление на ноль!")
+    elif c == '//':
+        if b != 0:
+            print(a, '//', b, '=', a // b)
+        else:
+            print("Ошибка: деление на ноль!")
+    elif c == '%':
+        if b != 0:
+            print(a, '%', b, '=', a % b)
+        else:
+            print("Ошибка: деление на ноль!")
+    else:
+        print("Ошибка: неверный символ операции. Пожалуйста, используйте +, -, *, **, /, //, % или 'v' для выхода.")

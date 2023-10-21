0a0c6bda59c209115971a446281382cddcd6d6f5
Ycanzzay <steam3bebra228@gmail.com>
2023-10-21 15:51:20 +0300

    Shopping_list

--- main.py
+++ main.py
@@ -1,20 +1,13 @@
-print('calculator')
-a = int(input('Введите число'))
-b = int(input('Введите число'))
-c = input('Введите символ + - * ** / // %')
-if c == '+':
-    print('a', '+', 'b', '=', a + b)
-if c == '-':
-    print('a', '-', 'b', '=', a - b)
-if c == '*':
-    print('a', '*', 'b', '=', a * b)
-if c == '**':
-    print('a', '**', 'b', '=', a ** b)
-if c == '/':
-    print('a', '/', 'b', '=', a / b)
-if c == '//':
-    print('a', '//', 'b', '=', a // b)
-if c == '%':
-    print('a', '%', 'b', '=', a % b)
-
+shopping_list = ["яблоко", "молоко", "хлеб", "яйца", "сок"]
+s = 0
+while s<len(shopping_list):
+    print(shopping_list[s])
+    s+=1
+shopping_list.remove("молоко")
+print(shopping_list)
+shopping_list.remove("яблоко")
+shopping_list.insert(0,"банан")
+print(shopping_list)
+for shopping_list in shopping_list:
+    print(shopping_list)
 

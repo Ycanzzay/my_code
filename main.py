fe70777881478f5b3776e4178e56b3889fed3653
Ycanzzay <steam3bebra228@gmail.com>
2023-12-12 22:58:04 +0300

    домашка с args

--- main.py
+++ main.py
@@ -1,13 +1,5 @@
-shopping_list = ["яблоко", "молоко", "хлеб", "яйца", "сок"]
-s = 0
-while s<len(shopping_list):
-    print(shopping_list[s])
-    s+=1
-shopping_list.remove("молоко")
-print(shopping_list)
-shopping_list.remove("яблоко")
-shopping_list.insert(0,"банан")
-print(shopping_list)
-for shopping_list in shopping_list:
-    print(shopping_list)
+def generate_points(*args):
+    return ['.' * num for num in args]
 
+result = generate_points(2, 4, 3)
+print(result)
